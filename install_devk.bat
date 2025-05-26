3daa7bc521cb7848e2f48dc18439847f0adcff96
kingtoad-c <demoacc4tv@gmail.com>
2025-05-26 11:55:06 +0100

    Update install_devk.bat

--- install_devk.bat
+++ install_devk.bat
@@ -11,15 +11,16 @@ echo Creating devk launcher at: %launcher_path%
 > "%launcher_path%" (
     echo @echo off
     echo REM os == windows
-    echo REM name == update 8.1
+    echo REM name == update v1.0
     echo REM req == python
-    echo REM creator == windows inc.
+    echo REM creator == kingtoad-c
     echo REM source == github
     echo set "driver_file=%%~nx1"
     echo title %%driver_file%%
     echo echo Starting program: %%driver_file%%
     echo cls
     echo curl -s https://raw.githubusercontent.com/kingtoad-c/sys/refs/heads/main/logic.py ^| python - "%%driver_file%%"
+    echo set /p "enter=click enter to finish..."
 )
 
 :: Add registry entries for .devk extension
@@ -29,7 +30,7 @@ reg add "HKEY_CLASSES_ROOT\.devk" /ve /d "devkfile" /f >nul
 reg add "HKEY_CLASSES_ROOT\devkfile\shell\open\command" /ve /d "\"%launcher_path%\" \"%%1\"" /f >nul
 
 echo.
-echo ✅ .devk files will now open with your devk launcher!
+echo .devk files will now open with the devk launcher!
 echo You can now double-click any .devk file to run it.
 pause
 endlocal
